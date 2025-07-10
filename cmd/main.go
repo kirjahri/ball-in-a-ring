6d2216054a52ff68bc20c36384149916b918ad60
kirjahri <150558369+kirjahri@users.noreply.github.com>
2025-07-10 10:06:39 -0400

    feat: create `Game` struct and constants

--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,39 @@ package main
 
 import rl "github.com/gen2brain/raylib-go/raylib"
 
+type Game struct{}
+
+const (
+	screenWidth  = 800
+	screenHeight = 450
+)
+
+func (g *Game) Init() {}
+
+func (g *Game) Update() {}
+
+func (g *Game) Draw() {
+	rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+}
+
 func main() {
-	rl.InitWindow(800, 450, "raylib [core] example - basic window")
+	rl.SetConfigFlags(rl.FlagWindowHighdpi)
+
+	rl.InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window")
 	defer rl.CloseWindow()
 
-	rl.SetTargetFPS(60)
+	rl.SetTargetFPS(160)
+
+	var game Game
+	game.Init()
 
 	for !rl.WindowShouldClose() {
+		game.Update()
+
 		rl.BeginDrawing()
 
 		rl.ClearBackground(rl.RayWhite)
-		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+		game.Draw()
 
 		rl.EndDrawing()
 	}
