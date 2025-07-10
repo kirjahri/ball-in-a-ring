39aadb622088b97b9e085ee956a855d3e2b54da9
kirjahri <150558369+kirjahri@users.noreply.github.com>
2025-07-10 10:11:37 -0400

    feat: create `Ball` struct and draw red ball in middle of screen

--- cmd/main.go
+++ cmd/main.go
@@ -2,19 +2,35 @@ package main
 
 import rl "github.com/gen2brain/raylib-go/raylib"
 
-type Game struct{}
+type Ball struct {
+	position rl.Vector2
+	radius   float32
+	color    rl.Color
+}
+
+type Game struct {
+	balls []Ball
+}
 
 const (
 	screenWidth  = 800
 	screenHeight = 450
 )
 
-func (g *Game) Init() {}
+func (g *Game) Init() {
+	g.balls = []Ball{{
+		position: rl.NewVector2(screenWidth/2, screenHeight/2),
+		radius:   20,
+		color:    rl.Red,
+	}}
+}
 
 func (g *Game) Update() {}
 
 func (g *Game) Draw() {
-	rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+	for i := range g.balls {
+		rl.DrawCircleV(g.balls[i].position, g.balls[i].radius, g.balls[i].color)
+	}
 }
 
 func main() {
